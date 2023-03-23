d5200d73ceae6084eef452abe7f8d2f251f1424f
monicakrishb <monica.krish@aspiresys.com>
2023-03-23 17:35:32 +0530

    added list of task

--- task/src/Components/List/TodoList.js
+++ task/src/Components/List/TodoList.js
@@ -1,32 +1,51 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/TodoList.css";
 import { CreateTask } from "../modals/CreateTask";
+import axios from "axios";
+import Cards from "./Cards";
 
-export const TodoList = () => {
-    const[modal,setModal]=useState(false);
-    const[taskList,setTaskList]=useState([]);
-    const toggle=()=>{
-        setModal(modal);
-    }
-    const saveTask=(taskObj)=>{
-        let tempList=taskList
-        tempList.push(taskObj)
-        setTaskList(tempList)
-        setModal(false)
-    }
+
+const TodoList = () => {
+  const [modal, setModal] = useState(false);
+
+  const [arraydata, setArraydata] = useState([]);
+
+  const toggle = () => {
+    setModal(modal);
+  };
+  useEffect(() => {
+    Data();
+  }, []);
+
+  const Data = async () => {
+    const response = await axios.get("http://localhost:8000/task");
+    console.log(response);
+    setArraydata(response.data);
+  };
+
   return (
     <>
-
       <div className="header text-center">
         <h3 id="color">Todo List</h3>
-        <button className="btn btn-primary mt-2" id="create" onClick={()=>setModal(true)}>
+        <button
+          className="btn btn-primary mt-2"
+          id="create"
+          onClick={() => setModal(true)}
+        >
           Create Task
         </button>
       </div>
+    <h1 className="title">Priority task</h1>
       <div className="task-container">
-
+        {arraydata &&
+          arraydata.map((e) => <Cards taskObj={e} />)}
       </div>
-      <CreateTask toggle={toggle} modal={modal} save={saveTask}/>
+      <div className="task-container">
+        {arraydata &&
+          arraydata.map((e) => <Cards taskObj={ e.priority=="high"} />)}
+      </div>
+      <CreateTask toggle={toggle} modal={modal} />
     </>
   );
 };
+export default TodoList;
