99e57eb47dc3430b4e9ee74570fa273e6ae7cb22
monicakrishb <monica.krish@aspiresys.com>
2023-03-24 17:19:47 +0530

    added service

--- task/src/services/API.js
+++ task/src/services/API.js
@@ -7,7 +7,16 @@ class HttpService {
   loginget = (email) => {
     return axios.get(this.environment + `/user?email=${email}`);
   };
-  
+   Data = async() => { 
+    const response=await axios.get(`${this.environment }/task`);
+    const data=response.data
+    console.log(data);
+   return data
+  // console.log(response.data);
+};
 }
+
+ 
+
 const service = new HttpService();
 export default service;
